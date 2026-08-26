d5ed80de71a575f4128e51eadf22cade5cf624c5
Mnêmê <mneme@foopgp.org>
2026-08-26 11:18:33 +0200

    L'eid s'ecrit colle, sans separateur
    JJB, 2026-08-26 : plus de « = » entre u4/u5 et le reste, partout, et toute
    exception doit se justifier. Les six en-tetes de copyright que nous avons
    poses ici le portaient.
    
    Intacts, volontairement : le commentaire de pgp2vcard.html qui documente le
    jeton deprecie « u4=VALUE », et une entree de changelog qui est de l'histoire.

--- aged.c
+++ aged.c
@@ -2,8 +2,8 @@
  * aged.c - Helpers and command drivers for the aged / dump-aged /
  *          clean-aged onak subcommands.
  *
- * Copyright 2026 Jean-Jacques Brucker (u4=sRyUhEbNU5OwyLEjfSwaXAe_42.17-002.76) <jjbrucker@foopgp.org>
- * Copyright 2026 Mneme (u5=001777236237.945e_43.30_005.38) <mneme@foopgp.org>
+ * Copyright 2026 Jean-Jacques Brucker (u4sRyUhEbNU5OwyLEjfSwaXAe_42.17-002.76) <jjbrucker@foopgp.org>
+ * Copyright 2026 Mneme (u5001777236237.945e_43.30_005.38) <mneme@foopgp.org>
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
